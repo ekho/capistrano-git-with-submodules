61b63a106529393b1a6d0f59d3cb372be75d3cd7
Boris Gorbylev <ekho@ekho.name>
2016-12-21 20:44:18 +0300

    restored origin archive_to_release_path behavior and added submodules checkout

--- lib/capistrano/scm/git-with-submodules.rb
+++ lib/capistrano/scm/git-with-submodules.rb
@@ -1,18 +1,14 @@
 require "capistrano/scm/git"
 
 class Capistrano::SCM::Git::WithSubmodules < Capistrano::SCM::Git
+  alias_method :origin_archive_to_release_path, :archive_to_release_path if method_defined?(:archive_to_release_path)
 
   def archive_to_release_path
-    fail ':repo_tree currently not supported by GitWithSubmodules' if fetch(:repo_tree)
+    origin_archive_to_release_path
 
+    return unless backend.test(:test, '-e', release_path) && backend.test("ls -A #{release_path} | read linevar")
 
-    return if backend.test(:test, '-e', release_path) && backend.test("ls -A #{release_path} | read linevar")
-
-    _clone_repo
-
-    backend.within_only release_path do
-      git :submodule, 'update', '--init', '--recursive'
-    end
+    submodules_to_release_path
 
     unless fetch(:git_keep_meta, false)
       verbose = Rake.application.options.trace ? 'v' : ''
@@ -20,39 +16,23 @@ class Capistrano::SCM::Git::WithSubmodules < Capistrano::SCM::Git
     end
   end
 
-  def _clone_repo
-    git_version = backend.capture(:git, '--version').strip.match('^git version (\d+(?:\.\d+)+)$')
-
-    if git_version.nil? || git_version[1] < '2.3.0'
-      _clone_repo_with_old_git
-    else
-      _clone_repo_with_fresh_git
-    end
-  end
-
-  def _clone_repo_with_fresh_git
-    git :clone, '--reference', repo_path.to_s, '--dissociate', (fetch(:git_keep_meta, false) ? '' : '--depth=1'), '-b', fetch(:branch), repo_url, release_path
-  end
-
-  def _clone_repo_with_old_git
-    git :clone, (fetch(:git_keep_meta, false) ? '' : '--depth=1'), '-b', fetch(:branch), "file://#{repo_path}", release_path
-    backend.within_only release_path do
-      git :remote, 'set-url', 'origin', repo_url
-    end
-  end
-end
-
-
-# shit hack to execute command only in specified directory
-module SSHKit
-  module Backend
-    class Abstract
-      def within_only(directory, &block)
-        pwd = @pwd
-        @pwd = []
-        within directory, &block
-      ensure
-        @pwd = pwd
+  ##
+  # Adds configured submodules recursively to release
+  # It does so by connecting the bare repo and the work tree using environment variables
+  # The reset creates a temporary index, but does not change the working directory
+  # The temporary index is removed after everything is done
+  def submodules_to_release_path
+    temp_index_file_path = release_path.join("INDEX_#{fetch(:release_timestamp)}")
+    backend.within "../releases/#{fetch(:release_timestamp)}" do
+      backend.with(
+          'GIT_DIR' => repo_path.to_s,
+          'GIT_WORK_TREE' => release_path.to_s,
+          'GIT_INDEX_FILE' => temp_index_file_path.to_s
+      ) do
+        git :reset, '--mixed', fetch(:branch)
+        git :submodule, 'update', '--init', '--depth', 1, '--checkout', '--recursive'
+        verbose = Rake.application.options.trace ? 'v' : ''
+        backend.execute :rm, "-f#{verbose}", temp_index_file_path.to_s
       end
     end
   end
