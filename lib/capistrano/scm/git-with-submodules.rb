d1a85bffe0db2c314353155c119ccf98ee224585
Jae-Mun Cha <amunsen@gmail.com>
2018-03-30 01:32:43 +0900

    Keep sync with .gitmodules (#16)

--- lib/capistrano/scm/git-with-submodules.rb
+++ lib/capistrano/scm/git-with-submodules.rb
@@ -28,6 +28,7 @@ class Capistrano::SCM::Git::WithSubmodules < Capistrano::Plugin
                 quiet = Rake.application.options.trace ? '' : '--quiet'
 
                 execute :git, :reset, '--mixed', quiet, fetch(:branch), '--'
+                execute :git, :submodule, 'sync', '--recursive', quiet
                 execute :git, :submodule, 'update', '--init', '--checkout', '--recursive', quiet
                 execute :find, release_path, "-name '.git'", "|",  "xargs -I {} rm -rf#{verbose} '{}'"
                 execute :rm, "-f#{verbose}", temp_index_file_path.to_s
