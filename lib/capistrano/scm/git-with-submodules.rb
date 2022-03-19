35dfdb794df036956b4bf0d652c7884b4ac64502
Scott Anderson <scott.anderson@scandiweb.com>
2022-03-19 11:56:17 +0200

    add-to-update-remote-fetch
    	- Added remote fetch so that actual latest commit gets fetched from remote

--- lib/capistrano/scm/git-with-submodules.rb
+++ lib/capistrano/scm/git-with-submodules.rb
@@ -29,7 +29,7 @@ class Capistrano::SCM::Git::WithSubmodules < Capistrano::Plugin
 
                 execute :git, :reset, '--mixed', quiet, fetch(:branch), '--'
                 execute :git, :submodule, 'sync', '--recursive', quiet
-                execute :git, :submodule, 'update', '--init', '--checkout', '--recursive', quiet
+                execute :git, :submodule, 'update', '--init', '--checkout', '--recursive', '--remote',  quiet
                 execute :find, release_path, "-name '.git'", "|",  "xargs -I {} rm -rf#{verbose} '{}'"
                 execute :rm, "-f#{verbose}", temp_index_file_path.to_s
               end if test :test, '-f', release_path.join('.gitmodules')
